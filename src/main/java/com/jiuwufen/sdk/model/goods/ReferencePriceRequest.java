277595b8136232438f3d35c3285149cae5e33bdd
huarun <hr564335@gmail.com>
2026-09-23 20:27:14 +0800

    feat(goods): 添加参考价格请求参数字段
    - 添加卖家订单号字段(order_number)
    - 添加skuId字段(sku_id)
    - 添加成色字段(is_new)
    - 添加出售方式字段(sale_type)，固定传值为3

--- src/main/java/com/jiuwufen/sdk/model/goods/ReferencePriceRequest.java
+++ src/main/java/com/jiuwufen/sdk/model/goods/ReferencePriceRequest.java
@@ -9,10 +9,34 @@ import java.util.List;
  */
 @Data
 public class ReferencePriceRequest {
-    
+
     /**
      * 商品编号
      */
     @SerializedName("goods_sn")
     private String goodsSn;
+
+    /**
+     * 卖家订单号
+     */
+    @SerializedName("order_number")
+    private String orderNumber;
+
+    /**
+     * skuId
+     */
+    @SerializedName("sku_id")
+    private Long skuId;
+
+    /**
+     * 成色
+     */
+    @SerializedName("is_new")
+    private Long isNew;
+
+    /**
+     * 出售方式 （固定传：sale_type = 3）
+     */
+    @SerializedName("sale_type")
+    private Integer saleType;
 }
